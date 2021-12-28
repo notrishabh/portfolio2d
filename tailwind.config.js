9bf44308503b8b27bc22ea8a79fc176364aea6c1
notrishabh <rishabh107107@gmail.com>
2021-12-28 18:55:17 +0530

    Installed icons, front page background image and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgMain: "#002B34",
+      },
+      fontFamily: {
+        Ruluko: ["Ruluko"],
+        Josefin: ["Josefin Slab"],
+      },
+    },
+  },
+  plugins: [],
+};
